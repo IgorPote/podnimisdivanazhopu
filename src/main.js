8d31e9ed80ff010b610b0b19448c0d2ea3bd7e13
IgorPote <zzeroinst@gmail.com>
2025-07-29 16:16:44 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Основной файл проекта podnimisdivanazhopu
+
+console.log('Проект podnimisdivanazhopu запущен!');
+
+// Здесь будет основная логика приложения
+function init() {
+    console.log('Инициализация приложения...');
+}
+
+// Запуск приложения
+init(); 
